9642b7973b6859a07aa6cc1f2a2e6bf2d88abb41
Renato Rosa Franco <renato_ny@live.com>
2024-07-07 00:54:27 -0300

    feature: book entity added.

---
+++ lib/book.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Book
+  attr_accessor :id, :title, :author, :published_date
+
+  def initialize(id, title, author, published_date)
+    @id = id
+    @title = title
+    @author = author
+    @published_date = published_date
+  end
+end
